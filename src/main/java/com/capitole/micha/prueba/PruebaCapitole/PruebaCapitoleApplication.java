8704a786b9c4376a17eed1d87c4be92dbd752d37
micha <micha2mc@gmail.com>
2025-12-15 15:25:12 +0100

    first commit

---
+++ src/main/java/com/capitole/micha/prueba/PruebaCapitole/PruebaCapitoleApplication.java
@@ -0,0 +1,13 @@
+package com.capitole.micha.prueba.PruebaCapitole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaCapitoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaCapitoleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capitole/micha/prueba/PruebaCapitole/PruebaCapitoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capitole.micha.prueba.PruebaCapitole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaCapitoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
